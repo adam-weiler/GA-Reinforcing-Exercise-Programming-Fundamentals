f432c330999178678aa6284d2b7ec914437c3a97
adam-weiler <adam.weiler@yahoo.ca>
2019-07-15 12:59:47 -0400

    Adding complete project.

---
+++ exercise0.py
@@ -0,0 +1,21 @@
+venues = [
+{ 'address': "123 Main Street", 'city': "Toronto", 'wheelchair_accessible': True, 'capacity': 100 },
+{ 'address': "567 Centre Street", 'city': "Toronto", 'wheelchair_accessible': False, 'capacity': 400 },
+{ 'address': "9B Ontario Street", 'city': "Montreal", 'wheelchair_accessible': True, 'capacity': 1000 },
+{ 'address': "56 Road Avenue", 'city': "Ottawa", 'wheelchair_accessible': True, 'capacity': 650 },
+{ 'address': "938 Avenue Way East", 'city': "Toronto", 'wheelchair_accessible': False, 'capacity': 90 },
+{ 'address': "34 Main Street West", 'city': "London", 'wheelchair_accessible': False, 'capacity': 300 },
+{ 'address': "44 Quebec Road", 'city': "Toronto", 'wheelchair_accessible': True, 'capacity': 200 },
+{ 'address': "10 Spruce Avenue Ouest", 'city': "Montreal", 'wheelchair_accessible': False, 'capacity': 525 }
+]
+
+def check_venues(venues): #Returns a list of venues that are wheelchair accessible, and can fit at least 150 people.
+    new_venues = []
+
+    for venue in venues:
+        if (venue['wheelchair_accessible'] and venue['capacity'] > 150): #If wheelchair_accessible is true, and capcity is over 150.
+            new_venues.append(venue)
+
+    return new_venues
+
+print(check_venues(venues))
